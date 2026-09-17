a619ea20a46b1975de23a45644fc972ecfb85244
Fventra <16526139@mahasiswa.itb.ac.id>
2026-09-17 22:24:58 +0700

    susah

--- latihan_07.py
+++ latihan_07.py
@@ -51,3 +51,70 @@
 # ============================================================
 
 # Tulis kode Python kamu di bawah ini
+jumlah_kursi = int(input("Masukkan kursi total yang tersedia: "))
+
+kursi = []
+status_kursi = []
+
+
+
+def tampilkan_kursi(jumlah_kursi):
+    for _ in range(jumlah_kursi):
+        kursi.append("Tersedia")
+
+    for index, status in enumerate(kursi):
+                status_kursi.append(f'kursi {index+1} {status}')
+
+    return (f'Kursi {index+1} {status}')
+
+def pesan_kursi(nomor):
+    status_kursi.remove(((nomor)-1))
+    status_kursi.insert(((nomor)-1), f'kursi {nomor} terisi')
+    return status_kursi(nomor)
+
+    
+    
+while True:
+
+    
+    print('''
+
+Selamat datang di XXI !!!
+1. Lihat Kursi
+2. Pesan Kursi
+3. Batalkan Kursi
+4. Keluar
+
+''')
+
+    opsi = int(input("Ingin buka apa? "))
+    if opsi == 1:
+
+        print(tampilkan_kursi(jumlah_kursi))
+
+    if opsi == 2:
+        
+        nomor = int(input("Beli kursi nomor berapa :"))
+        pesan_kursi(nomor)
+
+         
+
+    
+
+
+
+
+
+
+
+
+
+
+
+#Loop kalau ingin meminta ketersediaan kursi
+for index, status in enumerate(kursi):
+    status_kursi.append(f'kursi {index} {status}')
+    print(f'Kursi {index+1} {status}')
+
+
+
